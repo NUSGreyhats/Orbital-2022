fd81f7ddec16dc421cd85ebf6cfe5c2a2df7365b
Wen Junhua <wenjh1998@gmail.com>
2022-05-31 00:27:55 +0800

    feat: add signup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,6 +8,7 @@ import Login from "./routes/login";
 import NavBar from "./components/navbar";
 import NewNote from "./routes/new";
 import ViewNote from "./routes/view";
+import Signup from "./routes/signup";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -18,6 +19,7 @@ root.render(
       <Route path="/login" element={<Login />} />
       <Route path="/new" element={<NewNote />} />
       <Route path="/view" element={<ViewNote />} />
+      <Route path="/signup" element={<Signup />} />
     </Routes>
   </BrowserRouter>
 );
