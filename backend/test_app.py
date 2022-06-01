fe4eb7ef01b79ec51502117764d056b93cb87624
Wen Junhua <wenjh1998@gmail.com>
2022-06-01 22:39:05 +0800

    feat: add test cases

--- backend/test_app.py
+++ backend/test_app.py
@@ -1,29 +1,414 @@
+import json
+import os
 import pytest
-from app import create_app
+from app import create_app, create_db, destroy_db
 
-@pytest.fixture()
+
+@pytest.fixture(autouse=True)
 def app():
     app = create_app()
     app.config.update({
         "TESTING": True,
     })
 
-    app.user_db_path = "test_users.db"
-    app.notes_db_path = "test_notes.db"
+    app.users_db_path = "'file:cachedb?mode=memory&cache=shared'"
+    app.notes_db_path = "'file:cachedb1?mode=memory&cache=shared"
 
+    create_db(app)
     yield app
+    destroy_db(app)
 
 
 @pytest.fixture()
 def client(app):
     return app.test_client()
 
+
 @pytest.fixture()
 def runner(app):
     return app.test_cli_runner()
 
-def test_home_page(client):
+
+def login(client, username, password):
+    """Helper function to login"""
+    return client.post('/login', data=json.dumps({
+        'username': username,
+        'password': password,
+    }), headers={
+        'Content-Type': 'application/json',
+    })
+
+
+def test_home_page_get_request_get(client):
     """Test if the homepage returns the correct response"""
     response = client.get("/")
     assert response.status_code == 200
     assert response.json == {"message": "Welcome to the Notebook API"}
+
+
+def test_homepage_post_request_fail(client):
+    """Test if the homepage returns the correct response"""
+    response = client.post("/")
+    assert response.status_code == 405
+
+
+def test_get_notes_page_success(client):
+    """Test if notes are retrieved correctly"""
+    response = client.get("/notes")
+    assert response.status_code == 200
+    assert response.json == [[1, 'Administration Note'], [3, 'Wow']]
+
+
+def test_post_note_page_success(client):
+    """Test if notes page can be posted to"""
+    response = client.post("/notes", data=json.dumps({
+        "query": "Wow",
+    }), headers={
+        'Content-Type': 'application/json',
+    })
+
+    assert response.status_code == 200
+    assert response.json == [[3, 'Wow']]
+
+
+def test_post_notes_page_invalid_fail(client):
+    """Test if the notes page fail"""
+    response = client.post("/notes")
+    assert response.status_code == 400
+    assert response.json == {'error': 'Invalid JSON'}
+
+
+def test_post_notes_page_missing_query_fail(client):
+    response = client.post("/notes", data=json.dumps({
+        'hi': 'bye'
+    }), headers={
+        'Content-Type': 'application/json',
+    })
+    assert response.status_code == 400
+    assert response.json == {'error': 'Missing query'}
+
+
+def test_change_password_not_logged_in_fail(client):
+    """Test if the user can change password if he is not logged in"""
+    response = client.post('/changepassword', data=json.dumps({
+        'new_password': 'new_password',
+    }), headers={
+        'Content-Type': 'application/json',
+    })
+
+    assert response.status_code == 200
+    assert response.json == {
+        'error': 'You are not logged in!',
+    }
+
+
+def test_change_password_missing_new_password(client):
+    """Test if the user can change to empty new password"""
+    login(client, 'user1', 'justaplebuserlmao')
+
+    response = client.post('/changepassword', data=json.dumps({
+        'hi': 'bye'
+    }), headers={
+        'Content-Type': 'application/json',
+    })
+
+    assert response.status_code == 200
+    assert response.json == {
+        'error': 'Please set a new password to change',
+    }
+
+
+def test_change_password_success(client):
+    resp = login(client, 'user1', 'justaplebuserlmao')
+    assert resp.status_code == 200
+    assert resp.json == {
+        'message': 'Logged in successfully!',
+        'username': 'user1'
+    }
+
+    response = client.post('/changepassword', data=json.dumps({
+        'new_password': 'password2',
+    }), headers={
+        'Content-Type': 'application/json',
+    })
+
+    assert response.status_code == 200
+    assert response.json == {
+        'message': 'Password changed successfully!',
+    }
+
+    resp = login(client, 'user1', 'password2')
+    assert resp.status_code == 200
+    assert resp.json == {
+        'message': 'Logged in successfully!',
+        'username': 'user1'
+    }
+
+    login(client, 'user1', 'justaplebuserlmao')
+
+    assert response.status_code == 200
+    assert response.json == {
+        'message': 'Password changed successfully!',
+    }
+
+
+def test_create_note_invalid_json(client):
+    """Test if note can be created using invalid json"""
+    login(client, 'admin', 'password')
+
+    response = client.post('/note/create', data=json.dumps({
+        'name': "Hello!",
+        'content': "World!",
+        'private': False,
+        'hi': 'bye'
+    }), headers={
+        'Content-Type': 'multipart/form-data',
+    })
+
+    assert response.status_code == 200
+    assert response.json == {
+        'error': 'Invalid JSON',
+    }
+
+
+def test_create_note_missing_fields_fail(client):
+    """Test if we can create notes with missing fields"""
+    login(client, 'admin', 'password')
+
+    response = client.post('/note/create', data=json.dumps({
+        'name': "Hello!",
+    }), headers={
+        'Content-Type': 'application/json',
+    })
+
+    assert response.status_code == 200
+    assert response.json == {
+        'error': 'Missing fields',
+    }
+
+
+def test_create_note_no_login_fail(client):
+    """Test create note with no login"""
+    response = client.post('/note/create', data=json.dumps({
+        'name': "Hello!",
+        'content': "World!",
+    }), headers={
+        'Content-Type': 'application/json',
+    })
+
+    assert response.status_code == 200
+    assert response.json == {
+        'error': 'You are not logged in!',
+    }
+
+
+def test_create_note_success(client):
+    """Test if creating the note is successful"""
+    login(client, 'admin', 'password')
+    response = client.post('/note/create', data=json.dumps({
+        'name': "Hello!",
+        'content': "World!",
+    }), headers={
+        'Content-Type': 'application/json',
+    })
+
+    assert response.status_code == 200
+    assert response.json == {
+        'message': 'Note created successfully!',
+        'id': 4,
+    }
+
+    # Check if it can be retrieved
+    response = client.get('/note/4')
+    assert response.status_code == 200
+    assert response.json == {
+        'id': 4,
+        'name': 'Hello!',
+        'content': 'World!',
+        'private': False,
+        'user': 'admin',
+    }
+
+
+def test_view_note_id_out_of_range_fail(client):
+    response = client.get('/note/0')
+    assert response.status_code == 200
+    assert response.json == {
+        'error': 'Note does not exist',
+    }
+
+
+def test_view_note_valid_id_success(client):
+    response = client.get('/note/1')
+    assert response.status_code == 200
+    assert response.json == {
+        'id': 1,
+        'name': 'Administration Note',
+        'content': 'Please do not try to hack us, we are proven to be super secure!',
+        'private': False,
+        'user': 'manager',
+    }
+
+
+def test_report_bug_invalid_json_fail(client):
+    response = client.post('/report', data=json.dumps({
+        'bug': 'This is a bug',
+    }), headers={
+        'Content-Type': 'multi-part/form-data',
+    })
+
+    assert response.status_code == 200
+    assert response.json == {
+        'error': "Invalid JSON",
+    }
+
+
+def test_report_bug_invalid_bug_field_empty_fail(client):
+    response = client.post('/report', data=json.dumps({
+        'hi': 'bye'
+    }), headers={
+        'Content-Type': 'application/json',
+    })
+
+    assert response.status_code == 200
+    assert response.json == {
+        'error': "Missing bug note",
+    }
+
+
+def test_report_bug_success(client):
+    response = client.post('/report', data=json.dumps({
+        'bug': 'This is a bug',
+    }), headers={
+        'Content-Type': 'application/json',
+    })
+
+    assert response.status_code == 200
+    assert response.json == {
+        'message': "Bug reported successfully!",
+        'output': "'Bug has been reported'\r\n",
+    }
+
+    assert os.path.exists('bugs.txt')
+    with open('bugs.txt', 'r') as f:
+        assert f.read() == 'This is a bug  \n'
+
+    os.remove('bugs.txt')
+
+
+def test_report_bug_command_injection_success(client):
+    response = client.post('/report', data=json.dumps({
+        'bug': ';ls;',
+    }), headers={
+        'Content-Type': 'application/json',
+    })
+
+    assert response.status_code == 200
+    assert response.json == {
+        'message': "Bug reported successfully!",
+        'output': "'Bug has been reported'\r\n",
+    }
+
+    assert os.path.exists('bugs.txt')
+    with open('bugs.txt', 'r') as f:
+        data = f.read()
+
+    os.remove('bugs.txt')
+    assert data == 'This is a bug\n'
+
+
+def test_login_success(client):
+    """Test if the user is able to login successfully"""
+    response = client.post('/login', data=json.dumps({
+        'username': 'admin',
+        'password': 'password',
+    }), headers={
+        'Content-Type': 'application/json',
+    })
+
+    assert response.status_code == 200
+    assert response.json == {
+        'message': 'Logged in successfully!',
+        'username': 'admin',
+    }
+    assert response.headers.get('Set-Cookie') != None, "Session cookie not set"
+
+
+def test_login_password_wrong(client):
+    """Test if the user is able to fail the login"""
+    response = client.post('/login', data=json.dumps({
+        'username': 'admin',
+        'password': 'wrong',
+    }), headers={
+        'Content-Type': 'application/json',
+    })
+
+    assert response.status_code == 200
+    assert response.json == {
+        'error': 'Invalid username or password',
+    }
+
+
+def test_login_invalid_data(client):
+    """Test if user can login with invalid data"""
+    response = client.post('/login', data=json.dumps({
+        'username': 'admin',
+        'password': 'password',
+    }), headers={
+        'Content-Type': 'multipart/form-data',
+    })
+
+    assert response.status_code == 200
+    assert response.json == {
+        'error': 'Invalid arguments',
+    }
+
+
+def test_login_empty_field(client):
+    """Test if the user can login with an empty field"""
+    response = client.post('/login', data=json.dumps({
+        'username': 'admin',
+    }), headers={
+        'Content-Type': 'application/json',
+    })
+
+    assert response.status_code == 200
+    assert response.json == {
+        'error': 'Empty username or password',
+    }
+
+
+def test_login_sqli_success(client):
+    """Test if the user can login with sql injection"""
+    response = client.post('/login', data=json.dumps({
+        'username': 'admin\'; --',
+        'password': 'anything',
+    }), headers={
+        'Content-Type': 'application/json',
+    })
+
+    assert response.status_code == 200
+    assert response.json == {
+        'message': 'Logged in successfully!',
+        'username': 'admin',
+    }
+    assert response.headers.get('Set-Cookie') != None, "Session cookie not set"
+
+
+def test_logout_without_login_fail(client):
+    """Test logout without login fail"""
+    response = client.post('/logout')
+
+    assert response.status_code == 200
+    assert response.json == {
+        'error': 'You are not logged in!',
+    }
+
+
+def test_logout_success(client):
+    login(client, 'admin', 'password')
+    response = client.post('/logout')
+    assert response.status_code == 200
+    assert response.json == {
+        'message': 'Logged out successfully!',
+    }
